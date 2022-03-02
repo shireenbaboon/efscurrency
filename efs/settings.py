b7417f95e4fc98775305e153e2cbb8ca5cbb3985
shireenbaboon <shireenbaboon@gmail.com>
2022-03-02 09:30:33 -0600

    forgot password added

--- efs/settings.py
+++ efs/settings.py
@@ -141,9 +141,18 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
-django_heroku.settings(locals())
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'shireenbaboon@gmail.com'
+EMAIL_HOST_PASSWORD = 'qjmmmthbtvwvnone'
+
+
+
 
 AUTH_USER_MODEL = 'users.CustomUser'
+django_heroku.settings(locals())
 
 
 
